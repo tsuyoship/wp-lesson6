e1c4a78efa8676f3604aa795dfc83d3b1f8e93ab
tsuyoshi YASUMATSU <yasumag@gmail.com>
2017-08-13 21:11:41 +0000

    headerの間違い探し

--- wp-content/themes/demo/header.php
+++ wp-content/themes/demo/header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); //html要素のlang属性を出力 ?>>
 <head>
-  <meta charset="<? bloginfo( 'charset' ); ?>">
-  <title><?php wp_title( '|', true, 'right' ); //ページタイトル出力 ?><?php bloginfo('name'); ?></title>
-  <link rel="stylesheet" href="<?php echo get_template_directiory_uri(); //使用中のテーマディレクトリのURLを出力 ?>/css/normalize.css">
-  <link rel="stylesheet" href="<?php echo get_stylesheet_uri();　//使用中テーマのスタイルシートURLを出力 ?>">
-  <link rel="pingback" href="<?php bloginfo( 'pingback_url' );  //ピングバックURLを出力 ?>">
-  <?php wp_head(); //wp_headはテーマの</head>タグの直前に必ず挿入いたします ?>
+  <meta charset="<?php bloginfo( 'charset' ); //文字エンコーディング情報を出力 ?>">
+  <title><?php wp_title( '|', true, 'right' ); //ページタイトルを出力 ?><?php bloginfo('name'); //サイト名を表示 ?></title>
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); //使用中テーマディレクトリのURLを出力 ?>/css/normalize.css">
+  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); //使用中テーマのスタイルシートURLを出力 ?>">
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); //ピングバックURLを出力 ?>">
+  <?php wp_head(); //wp_headはテーマの</head>タグ直前に必ず挿入します ?>
 </head>
 <body <?php body_class(); //bodyタグにページの種類に応じたクラス名を与える ?>>
   <header>
@@ -15,7 +15,7 @@
     </div>
     <nav>
       <ul>
-        <li><a href="<?php echo home_url(); //トップページのurlを出力 ?>">TOP</a></li>
+        <li><a href="<?php echo home_url(); //トップページのURLを出力 ?>">TOP</a></li>
         <li><a href="<?php echo home_url(); ?>/about/">ABOUT</a></li>
         <li><a href="<?php echo home_url(); ?>/map/">MAP</a></li>
         <li><a href="<?php echo home_url(); ?>/blog/">BLOG</a></li>
@@ -23,9 +23,8 @@
       </ul>
     </nav>
   </header>
-    <div id="cover">
+  <div id="cover">
     <div id="slide">
       <img src="<?php echo get_template_directory_uri(); ?>/images/slide-1.png" alt="Welcome to our website!">
     </div>
   </div>
-  
